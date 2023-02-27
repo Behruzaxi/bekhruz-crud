fbcfdd4c9b27e945a2f6f68e115ada62a985408c
Behruzaxi <ebehruz846@gmail.com>
2023-02-27 20:38:08 +0500

    fds

--- src/components/GetData/GetData.jsx
+++ src/components/GetData/GetData.jsx
@@ -1,3 +1,79 @@
+// import axios from 'axios'
+// import "./style.css";
+// import React, { useEffect, useState } from 'react'
+// import { Container, Row, Col, Table } from 'react-bootstrap'
+
+// export default function GetData() {
+
+// const [user, setUser] = useState([])
+
+// const getAllData = () => {
+//     axios.get("http://localhost:3000/data").then((data) => {
+//         setUser(data.data)
+//     })
+// }
+
+// const delete_func = (id) => {
+//     axios.delete(`http://localhost:3000/data/${id}`)
+// }
+
+// useEffect(() => {
+//     getAllData()
+// }, [])
+
+//     return (
+//         <>
+//             <Container>
+//                 <Row className='justify-content-center mt-5'>
+//                     <Col xs={10}>
+//                         <Table striped bordered hover>
+//                             <thead>
+//                                 <tr>
+//                                     <th>#</th>
+//                                     <th>First Name</th>
+//                                     <th>Location</th>
+//                                     <th>Price</th>
+//                                     <th>Sale</th>
+//                                     <th>Img</th>
+//                                 </tr>
+//                             </thead>
+//                             <tbody className='tble'>
+
+//                                 {user?.map((data) => {
+//                                     return (
+//                                         <>
+//                                 <tr style={{paddingTop: "50px"}}>
+//                                     <td style={{fontSize: "30px", paddingTop: "40px"}}>{data.id}</td>
+//                                     <td style={{fontSize: "30px", paddingTop: "40px"}}>{data.name}</td>
+//                                     <td style={{fontSize: "30px", paddingTop: "40px"}}>{data.location}</td>
+//                                     <td style={{fontSize: "30px", paddingTop: "40px"}}>{data.price}</td>
+//                                     <td style={{fontSize: "30px", paddingTop: "40px"}}>{data.sale}</td>
+//                                     <img style={{width: "200px", height: "140px"}} src={data.img} alt="" />
+//                                     <button style={{marginLeft: "30px", padding: "10px, 10px ,10px, 10px", backgroundColor: "red", borderRadius: "8px", color: "white"}}  onClick={() => delete_func(data.id)}>delete</button>
+//                                 </tr>   
+//                                         </>
+//                                     )
+//                                 })}
+
+
+//                             </tbody>
+//                         </Table>
+//                     </Col>
+//                 </Row>
+//             </Container>
+//         </>
+//     )
+// }
+
+
+
+
+
+
+
+
+
+
 import axios from 'axios'
 import "./style.css";
 import React, { useEffect, useState } from 'react'
@@ -30,11 +106,9 @@ useEffect(() => {
                             <thead>
                                 <tr>
                                     <th>#</th>
-                                    <th>First Name</th>
-                                    <th>Location</th>
-                                    <th>Price</th>
-                                    <th>Sale</th>
-                                    <th>Img</th>
+                                    <th> Name</th>
+                                    <th> Email</th>
+                                    <th>Password</th>
                                 </tr>
                             </thead>
                             <tbody className='tble'>
@@ -45,10 +119,8 @@ useEffect(() => {
                                 <tr style={{paddingTop: "50px"}}>
                                     <td style={{fontSize: "30px", paddingTop: "40px"}}>{data.id}</td>
                                     <td style={{fontSize: "30px", paddingTop: "40px"}}>{data.name}</td>
-                                    <td style={{fontSize: "30px", paddingTop: "40px"}}>{data.location}</td>
-                                    <td style={{fontSize: "30px", paddingTop: "40px"}}>{data.price}</td>
-                                    <td style={{fontSize: "30px", paddingTop: "40px"}}>{data.sale}</td>
-                                    <img style={{width: "140px", height: "140px"}} src={data.img} alt="" />
+                                    <td style={{fontSize: "30px", paddingTop: "40px"}}>{data.email}</td>
+                                    <td style={{fontSize: "30px", paddingTop: "40px"}}>{data.password}</td>
                                     <button style={{marginLeft: "30px", padding: "10px, 10px ,10px, 10px", backgroundColor: "red", borderRadius: "8px", color: "white"}}  onClick={() => delete_func(data.id)}>delete</button>
                                 </tr>   
                                         </>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,86 @@
+// import React from 'react'
+// import {  Button, Col, Container, Form, Row } from 'react-bootstrap'
+// import { useState } from 'react'
+// import axios from 'axios'
+// import { useNavigate } from 'react-router-dom'
+
+// export default function Header() {
+
+//     const [name, setName] = useState("")
+//     const [location, setLocation] = useState("")
+//     const [price, setPrice] = useState("")
+//     const [sale, setSale] = useState("")
+//     const [img, setImg] = useState("")
+//     const navigate = useNavigate()
+
+    
+//     let obj = {
+//         name: name,
+//         location: location,
+//         price: price,
+//         sale: sale,
+//         img: img
+//     }
+    
+//     const postdata = (e) => {
+//         e.preventDefault()
+//         axios.post("http://localhost:3000/data", obj).then(res => {
+//             if(res.status === 201){
+//                 alert(res.statusText );
+//                  setName("")
+//                  setLocation("")
+//                  setPrice("")
+//                  setSale("")
+//                  setImg("")
+//                  navigate("/table")
+//             }
+//         })
+//     }
+
+
+
+//     return (
+//         <>
+//             <Container fluid="md">
+//                 <Row className='justify-content-center mt-5'>
+//                     <Col xs={6}>
+//                         <Form onSubmit={postdata}>
+//                             <Form.Group className="mb-3" controlId="formBasicEmail">
+//                                 <Form.Label>Name</Form.Label>
+//                                 <Form.Control type="text" placeholder="name"  onChange={(e) => setName(e.target.value)} value={name} required/>
+//                             </Form.Group>
+//                             <Form.Group className="mb-3" controlId="formBasicEmail">
+//                                 <Form.Label>Location</Form.Label>
+//                                 <Form.Control type="text" placeholder="location"  onChange={(e) => setLocation(e.target.value)} value={location} required/>
+//                             </Form.Group>
+//                             <Form.Group className="mb-3" controlId="formBasicEmail">
+//                                 <Form.Label>price</Form.Label>
+//                                 <Form.Control type="number" placeholder="price"  onChange={(e) => setPrice(e.target.value)} value={price} required/>
+//                             </Form.Group>
+//                             <Form.Group className="mb-3" controlId="formBasicEmail">
+//                                 <Form.Label>sale</Form.Label>
+//                                 <Form.Control type="number" placeholder="sale"  onChange={(e) => setSale(e.target.value)} value={sale} required/>
+//                             </Form.Group>
+//                             <Form.Group className="mb-3" controlId="formBasicEmail">
+//                                 <Form.Label >img</Form.Label>
+//                                 <Form.Control  type="text" placeholder="image-link"  onChange={(e) => setImg(e.target.value)} value={img} required/>
+//                             </Form.Group>
+//                             <Button variant="primary" style={{width: "100%"}} type="submit">
+//                                 Submit
+//                             </Button>
+//                         </Form>
+//                     </Col>
+//                 </Row>
+//             </Container>
+//         </>
+//     )
+// }
+
+
+
+
+
+
 import React from 'react'
 import {  Button, Col, Container, Form, Row } from 'react-bootstrap'
 import { useState } from 'react'
@@ -7,19 +90,15 @@ import { useNavigate } from 'react-router-dom'
 export default function Header() {
 
     const [name, setName] = useState("")
-    const [location, setLocation] = useState("")
-    const [price, setPrice] = useState("")
-    const [sale, setSale] = useState("")
-    const [img, setImg] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
     const navigate = useNavigate()
 
     
     let obj = {
         name: name,
-        location: location,
-        price: price,
-        sale: sale,
-        img: img
+        email: email,
+        password: password
     }
     
     const postdata = (e) => {
@@ -28,10 +107,8 @@ export default function Header() {
             if(res.status === 201){
                 alert(res.statusText );
                  setName("")
-                 setLocation("")
-                 setPrice("")
-                 setSale("")
-                 setImg("")
+                 setEmail("")
+                 setPassword("")
                  navigate("/table")
             }
         })
@@ -50,20 +127,12 @@ export default function Header() {
                                 <Form.Control type="text" placeholder="name"  onChange={(e) => setName(e.target.value)} value={name} required/>
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Location</Form.Label>
-                                <Form.Control type="text" placeholder="location"  onChange={(e) => setLocation(e.target.value)} value={location} required/>
+                                <Form.Label>email</Form.Label>
+                                <Form.Control type="email" placeholder="***@gmail.com"  onChange={(e) => setEmail(e.target.value)} value={email} required/>
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>price</Form.Label>
-                                <Form.Control type="number" placeholder="price"  onChange={(e) => setPrice(e.target.value)} value={price} required/>
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>sale</Form.Label>
-                                <Form.Control type="number" placeholder="sale"  onChange={(e) => setSale(e.target.value)} value={sale} required/>
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label >img</Form.Label>
-                                <Form.Control  type="text" placeholder="image-link"  onChange={(e) => setImg(e.target.value)} value={img} required/>
+                                <Form.Label>password</Form.Label>
+                                <Form.Control type="number" placeholder="*****"  onChange={(e) => setPassword(e.target.value)} value={password} required/>
                             </Form.Group>
                             <Button variant="primary" style={{width: "100%"}} type="submit">
                                 Submit
